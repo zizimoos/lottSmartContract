171c9a755949322503e9216b5ae81d05ad5f0a89
zizimoos_iMac <zizimoos@gmail.com>
2020-08-30 11:34:32 +0900

    ...

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -1,10 +1,14 @@
 const Lottery = artifacts.require("Lottery");
 const assertRevert = require("./assertRevert");
+const expectEvent = require("./expectEvent");
 
 contract("Lottery", function ([deployer, user1, user2]) {
   let lottery;
+  let betAmount = 5 * 10 ** 15;
+  let bet_block_interval = 3;
+
   beforeEach(async () => {
-    console.log("Before Each");
+    // console.log("Before Each");
     lottery = await Lottery.new();
   });
 
@@ -13,7 +17,7 @@ contract("Lottery", function ([deployer, user1, user2]) {
     assert.equal(0, pot);
   });
 
-  describe("Bet", function () {
+  describe.only("Bet", function () {
     it("should fail when the bet money is not 0.005ETH", async () => {
       //FAIL transaction
       await assertRevert(
@@ -21,12 +25,33 @@ contract("Lottery", function ([deployer, user1, user2]) {
       );
       // transaction object {chainId, value, to, from, gas(Limit), gasPrice}
     });
-    it.only("should put th bet to the bet queue with 1 bet", async () => {
+    it("should put the bet to the bet queue with 1 bet", async () => {
       // BET
-      await lottery.bet("0xab", { from: user1, value: 5000000000000000 });
+      let receipt = await lottery.bet("0xab", {
+        from: user1,
+        value: betAmount,
+      });
+      // console.log(receipt);
+
+      let pot = await lottery.getPot();
+      assert.equal(pot, 0);
+
       // check CONTRACT balance == 0.005
+      let contractBalance = await web3.eth.getBalance(lottery.address);
+      assert.equal(contractBalance, betAmount);
+
+      let currentBlockNumber = await web3.eth.getBlockNumber();
+      let bet = await lottery.getBetInfo(0);
+
+      assert.equal(
+        bet.answerBlockNumber,
+        currentBlockNumber + bet_block_interval
+      );
+      assert.equal(bet.bettor, user1);
+      assert.equal(bet.challenges, "0xab");
       // check bet info
       // check log
+      await expectEvent.inLogs(receipt.logs, "BET");
     });
   });
 });
