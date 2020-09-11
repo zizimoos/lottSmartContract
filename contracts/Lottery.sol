1f4924d982f8972430e9e60c86a81f451bc637c7
zizimoos_iMac <zizimoos@gmail.com>
2020-09-11 19:16:46 +0900

    done

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -14,20 +14,20 @@ contract Lottery {
 
     address payable public owner;
 
+    uint256 private _pot;
+    bool private mode = false; // false : use answer for test , true : use real block hash
+    bytes32 public answerForTest;
+
     uint256 internal constant BLOCK_LIMIT = 256;
     uint256 internal constant BET_BLOCK_INTERVAL = 3;
     uint256 internal constant BET_AMOUNT = 5 * 10**15;
-    uint256 private _pot;
-
-    bool private mode = false; // false: test mode, true: real mode
-    bytes32 public answerForTest;
 
     enum BlockStatus {Checkable, NotRevealed, BlockLimitPassed}
     enum BettingResult {Fail, Win, Draw}
 
     event BET(
         uint256 index,
-        address bettor,
+        address indexed bettor,
         uint256 amount,
         bytes1 challenges,
         uint256 answerBlockNumber
@@ -68,35 +68,43 @@ contract Lottery {
         owner = msg.sender;
     }
 
-    function getSomeValue() public pure returns (uint256 value) {
-        return 5;
-    }
-
     function getPot() public view returns (uint256 pot) {
         return _pot;
     }
 
-    /**
-     * @dev 배팅과 정답체크를 한다. 유저는 0.005 ETH를 보내야 한다. 배팅용 1 byte 글자를 보낸다.
-     * 큐에 저장된 베팅 정보는 이후 distrubute 함수에서 해결된다.
-     * @param challenges 유저가 배팅하는 글자.
-     * @return result 함수가 잘 수행 되었는 지 확인하는 bool 값.
+    /*
+     * @dev 베팅과 정답 체크를 한다. 유저는 0.005 ETH를 보내야 하고, 베팅용 1 byte 글자를 보낸다.
+     * 큐에 저장된 베팅 정보는 이후 distribute 함수에서 해결된다.
+     * @param challenges 유저가 베팅하는 글자
+     * @return 함수가 잘 수행되었는지 확인해는 bool 값
      */
-
     function betAndDistribute(bytes1 challenges)
         public
         payable
         returns (bool result)
     {
         bet(challenges);
-        distrubute();
+
+        distribute();
+
         return true;
     }
 
+    // 90846 -> 75846
+    /*
+     * @dev 베팅을 한다. 유저는 0.005 ETH를 보내야 하고, 베팅용 1 byte 글자를 보낸다.
+     * 큐에 저장된 베팅 정보는 이후 distribute 함수에서 해결된다.
+     * @param challenges 유저가 베팅하는 글자
+     * @return 함수가 잘 수행되었는지 확인해는 bool 값
+     */
     function bet(bytes1 challenges) public payable returns (bool result) {
-        //check the proper ether was sent
-        require(msg.value == BET_AMOUNT, "not enough ETH");
+        // Check the proper ether is sent
+        require(msg.value == BET_AMOUNT, "Not enough ETH");
+
+        // Push bet to the queue
         require(pushBet(challenges), "Fail to add a new Bet Info");
+
+        // Emit event
         emit BET(
             _tail - 1,
             msg.sender,
@@ -104,19 +112,16 @@ contract Lottery {
             challenges,
             block.number + BET_BLOCK_INTERVAL
         );
-        //push log
-        //event log
+
         return true;
     }
 
-    //save the bet to the queue
-
-    //Distribute
     /**
-     * @dev 베팅 결과 값을 확인 하고 팟머니를 분배한다.
-     * 정답실퍠: 팟머니 축적, 정답 맞춤 : 팟머니 획득, 한글자 맞춤 혹은 정답확인 불가 : 베팅금액만 획득
+     * @dev 베팅 결과값을 확인 하고 팟머니를 분배한다.
+     * 정답 실패 : 팟머니 축척, 정답 맞춤 : 팟머니 획득, 한글자 맞춤 or 정답 확인 불가 : 베팅 금액만 획득
      */
-    function distrubute() public {
+    function distribute() public {
+        // head 3 4 5 6 7 8 9 10 11 12 tail
         uint256 cur;
         uint256 transferAmount;
 
@@ -127,7 +132,7 @@ contract Lottery {
         for (cur = _head; cur < _tail; cur++) {
             b = _bets[cur];
             currentBlockStatus = getBlockStatus(b.answerBlockNumber);
-            //checkable : block.number > AnswerBlockNumber && block.number < BLOCK_LIMIT + AnswerBlockNumber
+            // Checkable : block.number > AnswerBlockNumber && block.number  <  BLOCK_LIMIT + AnswerBlockNumber 1
             if (currentBlockStatus == BlockStatus.Checkable) {
                 bytes32 answerBlockHash = getAnswerBlockHash(
                     b.answerBlockNumber
@@ -140,9 +145,11 @@ contract Lottery {
                         b.bettor,
                         _pot + BET_AMOUNT
                     );
+
                     // pot = 0
                     _pot = 0;
-                    // emit event WIN
+
+                    // emit WIN
                     emit WIN(
                         cur,
                         b.bettor,
@@ -152,11 +159,11 @@ contract Lottery {
                         b.answerBlockNumber
                     );
                 }
-                // if fail, bettor money goes to pot
+                // if fail, bettor's money goes pot
                 if (currentBettingResult == BettingResult.Fail) {
                     // pot = pot + BET_AMOUNT
                     _pot += BET_AMOUNT;
-                    // emit event FAIL
+                    // emit FAIL
                     emit FAIL(
                         cur,
                         b.bettor,
@@ -166,6 +173,7 @@ contract Lottery {
                         b.answerBlockNumber
                     );
                 }
+
                 // if draw, refund bettor's money
                 if (currentBettingResult == BettingResult.Draw) {
                     // transfer only BET_AMOUNT
@@ -173,7 +181,8 @@ contract Lottery {
                         b.bettor,
                         BET_AMOUNT
                     );
-                    // emit event DRAW
+
+                    // emit DRAW
                     emit DRAW(
                         cur,
                         b.bettor,
@@ -184,15 +193,17 @@ contract Lottery {
                     );
                 }
             }
-            //not revealed: block.number <= AnswerBlockNumber
+
+            // Not Revealed : block.number <= AnswerBlockNumber 2
             if (currentBlockStatus == BlockStatus.NotRevealed) {
                 break;
             }
-            //block limit passed: block.number >= AnswerBlockNumber + BLOCK_LIMIT
+
+            // Block Limit Passed : block.number >= AnswerBlockNumber + BLOCK_LIMIT 3
             if (currentBlockStatus == BlockStatus.BlockLimitPassed) {
-                //refund
+                // refund
                 transferAmount = transferAfterPayingFee(b.bettor, BET_AMOUNT);
-                //emit refund
+                // emit refund
                 emit REFUND(
                     cur,
                     b.bettor,
@@ -201,8 +212,8 @@ contract Lottery {
                     b.answerBlockNumber
                 );
             }
+
             popBet(cur);
-            //check the answer
         }
         _head = cur;
     }
@@ -214,10 +225,13 @@ contract Lottery {
         // uint256 fee = amount / 100;
         uint256 fee = 0;
         uint256 amountWithoutFee = amount - fee;
+
         // transfer to addr
         addr.transfer(amountWithoutFee);
+
         // transfer to owner
         owner.transfer(fee);
+
         return amountWithoutFee;
     }
 
@@ -238,31 +252,34 @@ contract Lottery {
         return mode ? blockhash(answerBlockNumber) : answerForTest;
     }
 
-    // @dev 배팅글자와 정답을 확인한다.
-    // @param challenges 베터가 배팅한 글자
-    // @param answer 블랙해쉬값
-    // @return 정답결과
-
+    /**
+     * @dev 베팅글자와 정답을 확인한다.
+     * @param challenges 베팅 글자
+     * @param answer 블락해쉬
+     * @return 정답결과
+     */
     function isMatch(bytes1 challenges, bytes32 answer)
         public
         pure
         returns (BettingResult)
     {
         // challenges 0xab
-        // answer 0xab ....ff 32bytes
+        // answer 0xab......ff 32 bytes
+
         bytes1 c1 = challenges;
         bytes1 c2 = challenges;
+
         bytes1 a1 = answer[0];
         bytes1 a2 = answer[0];
 
-        //get first number
-        c1 = c1 >> 4; //0xab -> 0x0a
-        c1 = c1 << 4; //0x0a -> 0xa0
+        // Get first number
+        c1 = c1 >> 4; // 0xab -> 0x0a
+        c1 = c1 << 4; // 0x0a -> 0xa0
 
         a1 = a1 >> 4;
         a1 = a1 << 4;
 
-        //get second number
+        // Get Second number
         c2 = c2 << 4; // 0xab -> 0xb0
         c2 = c2 >> 4; // 0xb0 -> 0x0b
 
@@ -272,12 +289,12 @@ contract Lottery {
         if (a1 == c1 && a2 == c2) {
             return BettingResult.Win;
         }
+
         if (a1 == c1 || a2 == c2) {
             return BettingResult.Draw;
         }
-        if (a1 != c1 && a2 != c2) {
-            return BettingResult.Fail;
-        }
+
+        return BettingResult.Fail;
     }
 
     function getBlockStatus(uint256 answerBlockNumber)
@@ -291,12 +308,15 @@ contract Lottery {
         ) {
             return BlockStatus.Checkable;
         }
+
         if (block.number <= answerBlockNumber) {
             return BlockStatus.NotRevealed;
         }
+
         if (block.number >= answerBlockNumber + BLOCK_LIMIT) {
             return BlockStatus.BlockLimitPassed;
         }
+
         return BlockStatus.BlockLimitPassed;
     }
 
@@ -317,12 +337,12 @@ contract Lottery {
 
     function pushBet(bytes1 challenges) internal returns (bool) {
         BetInfo memory b;
-        b.bettor = msg.sender; // 20byte 20000gas
-        b.answerBlockNumber = block.number + BET_BLOCK_INTERVAL; //32byte  20000gas
-        b.challenges = challenges; //byte
+        b.bettor = msg.sender; // 20 byte
+        b.answerBlockNumber = block.number + BET_BLOCK_INTERVAL; // 32byte  20000 gas
+        b.challenges = challenges; // byte // 20000 gas
 
         _bets[_tail] = b;
-        _tail++; //32byte 20000gas 또 값을 바꿀때 5000gas
+        _tail++; // 32byte 값 변화 // 20000 gas -> 5000 gas
 
         return true;
     }
--- test/lottery.test.js
+++ test/lottery.test.js
@@ -1,34 +1,31 @@
 const Lottery = artifacts.require("Lottery");
 const assertRevert = require("./assertRevert");
 const expectEvent = require("./expectEvent");
-const { assert } = require("chai");
 
 contract("Lottery", function ([deployer, user1, user2]) {
   let lottery;
   let betAmount = 5 * 10 ** 15;
   let betAmountBN = new web3.utils.BN("5000000000000000");
   let bet_block_interval = 3;
-
   beforeEach(async () => {
-    // console.log("Before Each");
     lottery = await Lottery.new();
   });
 
   it("getPot should return current pot", async () => {
     let pot = await lottery.getPot();
-    assert.equal(0, pot);
+    assert.equal(pot, 0);
   });
 
-  describe("Bet", function () {
-    it("should fail when the bet money is not 0.005ETH", async () => {
-      //FAIL transaction
+  describe.only("Bet", function () {
+    it("should fail when the bet money is not 0.005 ETH", async () => {
+      // Fail transaction
       await assertRevert(
         lottery.bet("0xab", { from: user1, value: 4000000000000000 })
       );
       // transaction object {chainId, value, to, from, gas(Limit), gasPrice}
     });
     it("should put the bet to the bet queue with 1 bet", async () => {
-      // BET
+      // bet
       let receipt = await lottery.bet("0xab", {
         from: user1,
         value: betAmount,
@@ -38,20 +35,21 @@ contract("Lottery", function ([deployer, user1, user2]) {
       let pot = await lottery.getPot();
       assert.equal(pot, 0);
 
-      // check CONTRACT balance == 0.005
+      // check contract balance == 0.005
       let contractBalance = await web3.eth.getBalance(lottery.address);
       assert.equal(contractBalance, betAmount);
 
+      // check bet info
       let currentBlockNumber = await web3.eth.getBlockNumber();
-      let bet = await lottery.getBetInfo(0);
 
+      let bet = await lottery.getBetInfo(0);
       assert.equal(
         bet.answerBlockNumber,
         currentBlockNumber + bet_block_interval
       );
       assert.equal(bet.bettor, user1);
       assert.equal(bet.challenges, "0xab");
-      // check bet info
+
       // check log
       await expectEvent.inLogs(receipt.logs, "BET");
     });
@@ -60,215 +58,196 @@ contract("Lottery", function ([deployer, user1, user2]) {
   describe("Distribute", function () {
     describe("When the answer is checkable", function () {
       it("should give the user the pot when the answer matches", async () => {
-        // 두 글자 다 맞을 때
+        // 두 글자 다 맞았을 때
         await lottery.setAnswerForTest(
-          "0xab98e92841b25739971d9ef4cc10ea8bc04ca521a30454bbfa0fc55ef1ada1b7",
+          "0xabec17438e4f0afb9cc8b77ce84bb7fd501497cfa9a1695095247daa5b4b7bcc",
           { from: deployer }
         );
-        // 1 -> 4
+
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
-        // 2 -> 5
+        }); // 1 -> 4
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
-        // 3 -> 6
+        }); // 2 -> 5
         await lottery.betAndDistribute("0xab", {
           from: user1,
           value: betAmount,
-        });
-        // 4 -> 7
+        }); // 3 -> 6
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
-        // 5 -> 8
+        }); // 4 -> 7
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
-        // 6 -> 9
+        }); // 5 -> 8
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
+        }); // 6 -> 9
 
-        let potBefore = await lottery.getPot(); // == 0.01 eth
+        let potBefore = await lottery.getPot(); //  == 0.01 ETH
         let user1BalanceBefore = await web3.eth.getBalance(user1);
 
-        // 7 -> 10 // user1 에게 pot이 간다.
         let receipt7 = await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
+        }); // 7 -> 10 // user1에게 pot이 간다
 
-        let potAfter = await lottery.getPot(); // == 0 eth
-        let user1BalanceAfter = await web3.eth.getBalance(user1); // == before + 0.015 eth
-        // betAndDistribute
+        let potAfter = await lottery.getPot(); // == 0
+        let user1BalanceAfter = await web3.eth.getBalance(user1); // == before + 0.015 ETH
 
-        // pot의 변화량 확인
+        // pot 의 변화량 확인
         assert.equal(
           potBefore.toString(),
           new web3.utils.BN("10000000000000000").toString()
         );
         assert.equal(potAfter.toString(), new web3.utils.BN("0").toString());
-        // assert.equal(potBefore);
+
+        // user(winner)의 밸런스를 확인
         user1BalanceBefore = new web3.utils.BN(user1BalanceBefore);
         assert.equal(
           user1BalanceBefore.add(potBefore).add(betAmountBN).toString(),
           new web3.utils.BN(user1BalanceAfter).toString()
         );
-        // user(winner)의 밸런스 확인
       });
-      it("should give the user the amount he or her bet when a single character matches", async () => {
-        // 한 글자 맞았을때
+
+      it("should give the user the amount he or she bet when a single character matches", async () => {
+        // 한 글자 다 맞았을 때
         await lottery.setAnswerForTest(
-          "0xab98e92841b25739971d9ef4cc10ea8bc04ca521a30454bbfa0fc55ef1ada1b7",
+          "0xabec17438e4f0afb9cc8b77ce84bb7fd501497cfa9a1695095247daa5b4b7bcc",
           { from: deployer }
         );
-        // 1 -> 4
+
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
-        // 2 -> 5
+        }); // 1 -> 4
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
-        // 3 -> 6
+        }); // 2 -> 5
         await lottery.betAndDistribute("0xaf", {
           from: user1,
           value: betAmount,
-        });
-        // 4 -> 7
+        }); // 3 -> 6
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
-        // 5 -> 8
+        }); // 4 -> 7
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
-        // 6 -> 9
+        }); // 5 -> 8
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
+        }); // 6 -> 9
 
-        let potBefore = await lottery.getPot(); // == 0.01 eth
+        let potBefore = await lottery.getPot(); //  == 0.01 ETH
         let user1BalanceBefore = await web3.eth.getBalance(user1);
 
-        // 7 -> 10 // user1 에게 pot이 간다.
         let receipt7 = await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
+        }); // 7 -> 10 // user1에게 pot이 간다
 
-        let potAfter = await lottery.getPot(); // == 0.01 eth
-        let user1BalanceAfter = await web3.eth.getBalance(user1); // == before + 0.005 eth
-        // betAndDistribute
+        let potAfter = await lottery.getPot(); // == 0.01 ETH
+        let user1BalanceAfter = await web3.eth.getBalance(user1); // == before + 0.005 ETH
 
-        // pot의 변화량 확인
+        // pot 의 변화량 확인
         assert.equal(potBefore.toString(), potAfter.toString());
 
-        // assert.equal(potBefore);
+        // user(winner)의 밸런스를 확인
         user1BalanceBefore = new web3.utils.BN(user1BalanceBefore);
         assert.equal(
           user1BalanceBefore.add(betAmountBN).toString(),
           new web3.utils.BN(user1BalanceAfter).toString()
         );
-        console.log(user1BalanceBefore.add(betAmountBN).toString());
-        // user(winner)의 밸런스 확인
       });
-      it.only("should get the eth of user when the answer does not matches at all", async () => {
-        // 두 글자 모두 틀렸을 때
+
+      it("should get the eth of user when the answer does not match at all", async () => {
+        // 다 틀렸을 때
         await lottery.setAnswerForTest(
-          "0xab98e92841b25739971d9ef4cc10ea8bc04ca521a30454bbfa0fc55ef1ada1b7",
+          "0xabec17438e4f0afb9cc8b77ce84bb7fd501497cfa9a1695095247daa5b4b7bcc",
           { from: deployer }
         );
-        // 1 -> 4
+
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
-        // 2 -> 5
+        }); // 1 -> 4
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
-        // 3 -> 6
-        await lottery.betAndDistribute("0xdf", {
+        }); // 2 -> 5
+        await lottery.betAndDistribute("0xef", {
           from: user1,
           value: betAmount,
-        });
-        // 4 -> 7
+        }); // 3 -> 6
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
-        // 5 -> 8
+        }); // 4 -> 7
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
-        // 6 -> 9
+        }); // 5 -> 8
         await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
+        }); // 6 -> 9
 
-        let potBefore = await lottery.getPot(); // == 0.01 eth
+        let potBefore = await lottery.getPot(); //  == 0.01 ETH
         let user1BalanceBefore = await web3.eth.getBalance(user1);
 
-        // 7 -> 10 // user1 에게 pot이 간다.
         let receipt7 = await lottery.betAndDistribute("0xef", {
           from: user2,
           value: betAmount,
-        });
+        }); // 7 -> 10 // user1에게 pot이 간다
 
-        let potAfter = await lottery.getPot(); // == 0.015 eth
+        let potAfter = await lottery.getPot(); // == 0.015 ETH
         let user1BalanceAfter = await web3.eth.getBalance(user1); // == before
-        // betAndDistribute
 
-        // pot의 변화량 확인
+        // pot 의 변화량 확인
         assert.equal(
           potBefore.add(betAmountBN).toString(),
           potAfter.toString()
         );
 
-        // assert.equal(potBefore);
-        // user(winner)의 밸런스 확인
+        // user(winner)의 밸런스를 확인
         user1BalanceBefore = new web3.utils.BN(user1BalanceBefore);
         assert.equal(
           user1BalanceBefore.toString(),
           new web3.utils.BN(user1BalanceAfter).toString()
         );
-        console.log(user1BalanceBefore.add(betAmountBN).toString());
       });
     });
-    describe("When the answer is not revealed(not mined)", function () {});
-    describe("When the answer is not revealed(block limit is passed)", function () {});
-  });
 
+    describe("When the answer is not revealed(Not Mined)", function () {});
+
+    describe("When the answer is not revealed(Block limit is passed)", function () {});
+  });
   describe("isMatch", function () {
     let blockHash =
-      "0xab98e92841b25739971d9ef4cc10ea8bc04ca521a30454bbfa0fc55ef1ada1b7";
-    it("should be bettingResult.win when two characters match", async () => {
+      "0xabec17438e4f0afb9cc8b77ce84bb7fd501497cfa9a1695095247daa5b4b7bcc";
+    it("should be BettingResult.Win when two characters match", async () => {
       let matchingResult = await lottery.isMatch("0xab", blockHash);
       assert.equal(matchingResult, 1);
     });
-    it("should be bettingResult.win when two characters match", async () => {
+
+    it("should be BettingResult.Fail when two characters match", async () => {
       let matchingResult = await lottery.isMatch("0xcd", blockHash);
       assert.equal(matchingResult, 0);
     });
-    it("should be bettingResult.win when two characters match", async () => {
-      let matchingResult = await lottery.isMatch("0xad", blockHash);
+
+    it("should be BettingResult.Draw when two characters match", async () => {
+      let matchingResult = await lottery.isMatch("0xaf", blockHash);
       assert.equal(matchingResult, 2);
+
       matchingResult = await lottery.isMatch("0xfb", blockHash);
       assert.equal(matchingResult, 2);
     });
